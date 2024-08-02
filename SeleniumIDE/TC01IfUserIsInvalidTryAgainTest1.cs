457ba347e47360edf07ddbaaaa60aef07b749aeb
EvgeniSimeonov <evgenisimeonov09@gmail.com>
2024-08-02 22:56:17 +0300

    added edge options

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -12,7 +12,14 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        driver = new EdgeDriver();
+        EdgeOptions options = new EdgeOptions();
+        options.AddArguments("headless");
+        options.AddArguments("no-sandbox");
+        options.AddArguments("disable-dev-shm-usage");
+        options.AddArguments("disable-gpu");
+        options.AddArguments("window-size=1920x1080");
+
+        driver = new EdgeDriver(options);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
     }
